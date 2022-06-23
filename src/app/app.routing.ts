4c293969e60ebe913592208f420fa103b400c2ca
Daniel Oliveira <82046811+DanielManaus@users.noreply.github.com>
2022-06-23 12:28:48 -0400

    Primeiro Envio de dados

---
+++ src/app/app.routing.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+import { LandingComponent } from './landing/landing.component';
+
+
+const routes: Routes =[
+    { path: 'home',             component: HomeComponent },
+    { path: 'landing',          component: LandingComponent },
+  
+    { path: '', redirectTo: 'landing', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: false
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
